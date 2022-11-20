1f22e352bef8e2915935a0420fb75a35ba77e2b2
SnO2WMaN <me@sno2wman.net>
2022-11-21 02:11:43 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  # main
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+    yamlfmt.url = "github:SnO2WMaN/yamlfmt.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            devshell.overlay
+            (final: prev: {yamlfmt = yamlfmt.packages.${system}.yamlfmt;})
+          ];
+        };
+      in {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            alejandra
+            treefmt
+            taplo-cli
+            yamlfmt
+          ];
+        };
+      }
+    );
+}
